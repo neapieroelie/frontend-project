b01c26b755dc0c30e7d057ebaacfab9c4fcc2f1c
NeaPieroelie <nea.pieroelie@gmail.com>
2024-10-08 19:59:02 -0700

    created the layout of the webapp

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,26 +8,27 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="app">
+          <aside className="navbar">
+            <div className="searchbar">
+              <form>
+                <input type="search" placeholder="search" />
+                <button type="button">Go</button>
+              </form>
+            </div>
+            <div className="categories">
+              <nav>
+                <button>Home</button>
+                <button>Catalog</button>
+                <button>All products</button>
+                <button>Wishlist</button>
+              </nav>
+            </div>
+          </aside>
+          <main>
+
+          </main>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
