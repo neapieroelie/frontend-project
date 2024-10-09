33f06bb8fd635abbbb13e49c622f8ccf7908b8cc
NeaPieroelie <nea.pieroelie@gmail.com>
2024-10-09 12:49:17 -0700

    styling the navigation bar

--- react/src/App.jsx
+++ react/src/App.jsx
@@ -8,7 +8,7 @@ function App() {
   return (
     <>
       <div className="app">
-          <Navbar/>
+          <Navbar />
           <main>
             <div className="">
 
--- react/src/components/navigation/Navigation.jsx
+++ react/src/components/navigation/Navigation.jsx
@@ -4,18 +4,20 @@ function Navbar () {
     return (
         <>
             <aside className={styles.navbar}>
-                <div className="searchbar">
+                <div className={styles.header}>
+                    <h1>Peekabook</h1>
+                </div>
+                <div className={styles.searchbar}>
                     <form>
                         <input type="search" placeholder="search" />
-                        <button type="button">Go</button>
                     </form>
                 </div>
                 <div className={styles.categories}>
                     <nav>
-                    <button>Discover</button>
-                    <button>Availability</button>
-                    <button>Genres</button>
-                    <button></button>
+                        <button>Discover</button>
+                        <button>Availability</button>
+                        <button>Genres</button>
+                        <button>test</button>
                     </nav>
                 </div>
             </aside>
